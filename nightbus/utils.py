ssssam/nightbus

commit 3513da19363b8b4564f199b469d13486996ede57

Fix multiple arguments passed to --hosts and --tasks
You can now pass the flag multiple times, or use a comma-separated list of
items. Previously only the last value would be used.

nightbus/utils.py
@@ -16,8 +16,18 @@
 '''Utility functions.'''
 
 
-def ensure_list(string_or_list_or_none):
+import itertools
+
+
+def ensure_list(string_or_list_or_none, separator=None):
     if isinstance(string_or_list_or_none, str):
-        return [string_or_list_or_none]
+        if separator:
+            return string_or_list_or_none.split(separator)
+        else:
+            return [string_or_list_or_none]
     else:
-        return string_or_list_or_none or []
+        if separator:
+            return list(itertools.chain.from_iterable(
+                item.split(separator) for item in string_or_list_or_none))
+        else:
+            return string_or_list_or_none or []

nightbus/__main__.py
@@ -37,10 +37,10 @@ def argument_parser():
         '--ignore-errors', action='store_true',
         help="Continue running tasks even if a previous task failed.")
     parser.add_argument(
-        '--hosts', '--host', type=str,
+        '--hosts', '--host', action='append',
         help="Select hosts to run on (default: all hosts)")
     parser.add_argument(
-        '--tasks', '--task', '-t', type=str,
+        '--tasks', '--task', '-t', action='append',
         help="Select tasks to run (default: all tasks)")
     parser.add_argument(
         '--log-directory', '-l', type=str, default='/var/log/ci',
@@ -114,7 +114,7 @@ def main():
         print("Available tasks:\n\n  *", '\n  * '.join(tasks.names()))
         return
 
-    hosts = ensure_list(args.hosts) or host_config.keys()
+    hosts = ensure_list(args.hosts, separator=',') or host_config.keys()
     tasks_to_run = ensure_list(args.tasks) or tasks.names()
     logging.info("Selected tasks: %s", ','.join(tasks_to_run))
 

commit 0829ca1bb133841efd9ff1753384b0895c1be924

Fix crash when no commandline args are passed
Regression from 3513da19363b8b4564f199b469d13486996ede57

nightbus/utils.py
@@ -25,9 +25,11 @@ def ensure_list(string_or_list_or_none, separator=None):
             return string_or_list_or_none.split(separator)
         else:
             return [string_or_list_or_none]
-    else:
+    elif string_or_list_or_none is not None:
         if separator:
             return list(itertools.chain.from_iterable(
                 item.split(separator) for item in string_or_list_or_none))
         else:
-            return string_or_list_or_none or []
+            return string_or_list_or_none
+    else:
+        return []